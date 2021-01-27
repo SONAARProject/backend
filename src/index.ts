60cf1735cec1dca211c7a1718ad59c5629595c3c
João Vicente <joao.vicente@campus.ul.pt>
2021-01-27 15:42:05 +0000

    updated code

--- src/index.ts
+++ src/index.ts
@@ -204,7 +204,7 @@ app.get("**", function (_, res: express.Response) {
   res.send("Invalid service");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT);
 console.log("Listening on port " + PORT);
